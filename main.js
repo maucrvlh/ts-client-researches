b0867cd14d07300ac3f31a28118f5b0ecf07b1ae
Mauricio Marinho <mauriciocmarinho@gmail.com>
2017-09-23 14:57:47 -0400

    removendo builds

--- main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var const_1 = require("./const");
-function hello(compiler) {
-    console.log("Hello, from " + compiler);
-}
-hello(const_1.compiler);
